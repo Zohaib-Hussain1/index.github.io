949feab90fc1940cdf27cfb1744655f56945dff7
unknown <you@example.com>
2024-03-26 23:03:40 +0500

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var weightInGrams = prompt("Enter the weight of chicken (in grams):");
+let weightInGrams = prompt("Enter the weight of chicken (in grams):");
 
-var weightInKg = weightInGrams / 1000;
+let weightInKg = weightInGrams / 1000;
 
-var price = weightInKg * 500;
+let price = weightInKg * 500;
 
 document.write("You have to pay " + price + " rupees for " + weightInGrams + " grams of chicken.");
